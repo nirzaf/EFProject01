d8ae6fb9eca79190a7f60def8524b4c52043a701
Fazrin <nirzaf@gmail.com>
2018-07-09 11:21:05 +0530

    Add project files.

---
+++ EFProject01/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFProject01.Startup))]
+namespace EFProject01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
